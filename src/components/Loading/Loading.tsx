2b1fc2e1f51e049675e28d0b85b83c913d8c5ba9
Benjamin Lacroix <benjamin.lacroix@publicissapient.com>
2021-07-15 21:56:07 +0200

    :dizzy: animate loader

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Loading = () => (
   <section className="select-none h-screen">
     <div className="flex justify-center items-center h-3/4">
-      <div className="font-game text-white text-base color-white text-shadow uppercase">Loading...</div>
+      <div className="font-game text-white text-base color-white text-shadow uppercase animate-pulse">Loading...</div>
     </div>
   </section>
 )
