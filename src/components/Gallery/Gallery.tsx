50e2ede45d478dbd24d7ce3f40148753eee77dc0
Guillaume Lhermenier <guillaume.lhermenier@gmail.com>
2022-01-24 01:21:25 +0100

    :sparkles: Add searchbar in Gallery

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,45 +1,73 @@
-import React, { FunctionComponent } from 'react';
-import { Metadata } from '../Metadata/Metadata';
-import useSWR from 'swr';
-import { GalleryCard } from './GalleryCard';
-import { Toolbar } from '../Toolbar/Toolbar';
-import { fetcher } from '../../services/fetch';
-import { Loading } from '../Loading/Loading';
+import React, {FunctionComponent, useEffect, useState} from 'react';
+import {Metadata} from '../Metadata/Metadata';
+import {GalleryCard} from "./GalleryCard";
+import {Toolbar} from "../Toolbar/Toolbar";
+import {Loading} from "../Loading/Loading";
+import {SearchBar} from "../Input/SearchBar";
+import {fetcher} from "../../services/fetch";
+import useSWR from "swr";
+import {Filter, getFilteredGallery, updateFilters, User} from "../../services/gallery";
 
-export interface User {
-  firstName: string;
-  lastName: string;
-  picture: string;
-}
 
-const useGallery = () => {
-  const { data } = useSWR(`/members`, fetcher, {
-    revalidateOnFocus: false,
-  });
-  return {
-    gallery: data,
-    isLoading: !data,
-  };
-};
+export const Gallery: FunctionComponent<{ location: Location }> = ({location}) => {
 
-export const Gallery: FunctionComponent<{ location: Location }> = ({ location }) => {
-  const { gallery, isLoading } = useGallery();
-  return (
-    <main className="mb-4 lg:mb-12">
-      <Metadata />
-      {!isLoading ? (
-        <>
-          <Toolbar title="Gallery" buttonLabel="Back" link={location.state.from} />
-          <div className="mx-3 my-4 flex flex-wrap items-center justify-center gap-3 md:gap-2 lg:gap-7">
-            <div className="w-full flex-grow text-center text-white">{gallery.length} members</div>
-            {gallery.map((user: User) => (
-              <GalleryCard key={user.picture} {...user} />
-            ))}
+    const [gallery, setGallery] = useState<User[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [filteredGallery, setFilteredGallery] = useState<User[]>(gallery)
+    const [stateFilters, setStateFilters] = useState<Filter[]>([])
+
+    const filterGallery = () => {
+      setFilteredGallery(getFilteredGallery(gallery, stateFilters));
+    }
+
+    const handleGalleryFiltering = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const {value, name} = event.target;
+      let updated_filters = updateFilters([...stateFilters], name, value);
+      setStateFilters(updated_filters);
+    }
+
+    const {data: member} = useSWR(`/members`, fetcher, {
+      onSuccess: (data, key, config) => {
+        setGallery(data);
+        setFilteredGallery(data);
+        setIsLoading(!data);
+      },
+      revalidateOnFocus: false
+    });
+
+    useEffect(() => {
+      filterGallery();
+    }, [stateFilters])
+
+    return (
+      <main className="mb-4 lg:mb-12">
+        <Metadata/>
+        {!isLoading ? <>
+          <Toolbar title="Gallery" buttonLabel="Back" link={location.state.from}/>
+          <div className="justify-center flex-col">
+            <section className="sticky flex-grow top-12 z-10 h-12 bg-blue-2 mx-4 mb-1">
+              <div className="flex justify-center items-center mx-4">
+                <SearchBar
+                  label=""
+                  placeholder="Search members"
+                  type="text"
+                  name="search"
+                  wide
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleGalleryFiltering(event)}
+                />
+              </div>
+            </section>
+            <section className="mx-8">
+              <div className="text-white text-right mx-4">{filteredGallery.length} members</div>
+            </section>
+            <section className="flex flex-wrap mx-3 my-3 gap-3 md:gap-2 lg:gap-7 justify-center">
+              {filteredGallery.map((user: User) => (
+                <GalleryCard key={user.picture} {...user}/>
+              ))}
+            </section>
           </div>
-        </>
-      ) : (
-        <Loading />
-      )}
-    </main>
-  );
-};
+        </> : <Loading/>}
+      </main>
+    );
+  }
+;
---
+++ src/services/gallery.ts
@@ -0,0 +1,73 @@
+export interface User {
+  firstName: string;
+  lastName: string;
+  picture: string;
+}
+
+export interface Filter {
+  name: string;
+  value: string;
+}
+
+const removeSigns = (value: string) => {
+  const allSigns = [
+    '"',  // U+0022
+    '\'', // U+0027
+    '‘', // U+2018
+    '’', // U+2019
+    '-', // hyphen
+    '_', // underscore
+  ]
+  return allSigns.reduce((value, signChar) => {
+    const re = new RegExp(signChar, 'g')
+    // Remove the signs
+    return value.replace(re, '');
+  }, value);
+}
+
+// Get rid of all glyph that may be added to all letters.
+// If will not affect what is stored in DB, it is only used for search filtering
+const removeDiacritics = (value: string) => value.normalize("NFD").replace(/\p{Diacritic}/gu, "");
+
+const normalizeString = (value: string) => removeSigns(removeDiacritics(value)).toLowerCase();
+
+const isMatchingSearch = (member: User, criteria: string) => {
+  const normalizedCriteria = normalizeString(criteria)
+  return normalizeString(member.firstName).startsWith(normalizedCriteria) || normalizeString(member.lastName).startsWith(normalizedCriteria);
+}
+
+export const updateFilters = (filters: Filter[], filter_name: string, value: string) => {
+  let updated = false;
+  let updated_filters = filters.map((filter: Filter) => {
+    if (filter.name === filter_name) {
+      let updated_filter = Object.assign({}, filter);
+      updated_filter.value = value;
+      updated = true;
+      return updated_filter;
+    } else {
+      return filter;
+    }
+  });
+  if (!updated) {
+    updated_filters.push({name: filter_name, value: value});
+  }
+  return updated_filters;
+}
+
+const isMemberMatchingCriteria = (member: User, filter: Filter) => {
+  const filterType: { [index: string]: any } = {
+    'search': isMatchingSearch(member, filter.value),
+  }
+  return filterType[filter.name];
+}
+
+const isMemberMatchingCriterias = (member: User, filters: Filter[]) => {
+  // Check all filters for this member and return whether it fits them all or not.
+  // When no filter is set, all members match.
+  return filters
+    .map((filter: Filter) => isMemberMatchingCriteria(member, filter))
+    .reduce((a, b) => a && b, true);
+}
+
+export const getFilteredGallery = (members: User[], filters: Filter[]) => members.filter((member: User) => isMemberMatchingCriterias(member, filters));
+
