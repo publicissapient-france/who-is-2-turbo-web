c7ff5fe820f3a68c66c203d1a3c29f6e135961f6
Benjamin Lacroix <benjamin.lacroix@publicissapient.com>
2022-03-07 16:07:56 +0100

    :sparkles: desktop gallery v2

--- src/components/Gallery/GalleryCard.tsx
+++ src/components/Gallery/GalleryCard.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import bgItemGallery from '../../images/bg-item-gallery.png';
+import bgItemGalleryDesktop from '../../images/bg-item-gallery-desktop.png';
 import newComerBadge from '../../images/new-comer-badge.svg';
 import { User } from '../../services/gallery';
 
@@ -9,19 +10,35 @@ export const GalleryCard: FunctionComponent<User> = ({ firstName, lastName, pict
   const year = date && date.getFullYear();
   const isNewComer = date && Math.floor((new Date().getTime() - date.getTime()) / (1000 * 60 * 60 * 24)) < 90;
   return (
-    <div className="relative mb-2 w-[328px]">
-      <img className="h-[149px] w-[328px]" src={bgItemGallery} alt="user's background" />
-      <img className="absolute top-[17px] left-[25px] h-[100px] w-[75px] shadow-inner" src={picture} alt="user's picture" />
-      <div className="absolute left-32 top-4">
-        <span className="mb-1 block font-game text-xs capitalize">{firstName}</span>
-        <span className="mb-2 block font-game text-xs capitalize">{lastName}</span>
-        {capability && <span className="mb-2 block text-base text-blue-3">{capability}</span>}
-        {date && (
-          <span className="text-sm text-grey-2">
-            {month} {year} {isNewComer && <img className="-mt-1 inline" src={newComerBadge} />}
-          </span>
-        )}
+    <>
+      <div className="relative mb-2 w-[328px] md:hidden">
+        <img className="h-[149px] w-[328px]" src={bgItemGallery} alt="user's background" />
+        <img className="absolute top-[17px] left-[25px] h-[100px] w-[75px] shadow-inner" src={picture} alt="user's picture" />
+        <div className="absolute left-32 top-4">
+          <span className="mb-1 block font-game text-xs capitalize">{firstName}</span>
+          <span className="mb-2 block font-game text-xs capitalize">{lastName}</span>
+          {capability && <span className="mb-2 block text-base text-blue-3">{capability}</span>}
+          {date && (
+            <span className="text-sm text-grey-2">
+              {month} {year} {isNewComer && <img className="-mt-1 inline" src={newComerBadge} />}
+            </span>
+          )}
+        </div>
       </div>
-    </div>
+      <div className="relative mb-2 hidden w-[231px] md:block">
+        <img className="h-[392px] w-[231px]" src={bgItemGalleryDesktop} alt="user's background" />
+        <img className="absolute top-[25px] left-[33px] h-[221px] w-[166px] shadow-inner" src={picture} alt="user's picture" />
+        <div className="absolute left-4 top-[270px]">
+          <span className="mb-1 block font-game text-xs capitalize">{firstName}</span>
+          <span className="mb-2 block font-game text-xs capitalize">{lastName}</span>
+          {capability && <span className="mb-2 block text-base text-blue-3">{capability}</span>}
+          {date && (
+            <span className="text-sm text-grey-2">
+              {month} {year} {isNewComer && <img className="-mt-1 inline" src={newComerBadge} />}
+            </span>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
