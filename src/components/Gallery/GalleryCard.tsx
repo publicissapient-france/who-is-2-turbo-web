0b7962cddf9685cfa35712bb8009401af5621b46
Benjamin Lacroix <benjamin.lacroix@publicissapient.com>
2022-03-07 17:53:21 +0100

    :lipstick: image border opacity

--- src/components/Gallery/GalleryCard.tsx
+++ src/components/Gallery/GalleryCard.tsx
@@ -13,7 +13,8 @@ export const GalleryCard: FunctionComponent<User> = ({ firstName, lastName, pict
     <>
       <div className="relative mb-2 w-[328px] md:hidden">
         <img className="h-[149px] w-[328px]" src={bgItemGallery} alt="user's background" />
-        <img className="absolute top-[17px] left-[25px] h-[100px] w-[75px] shadow-inner" src={picture} alt="user's picture" />
+        <img className="absolute top-[17px] left-[25px] h-[100px] w-[75px]" src={picture} alt="user's picture" />
+        <div className="absolute top-[17px] left-[25px] h-[100px] w-[75px] border-2 opacity-30" />
         <div className="absolute left-32 top-4">
           <span className="mb-1 block font-game text-xs capitalize">{firstName}</span>
           <span className="mb-2 block font-game text-xs capitalize">{lastName}</span>
@@ -27,7 +28,8 @@ export const GalleryCard: FunctionComponent<User> = ({ firstName, lastName, pict
       </div>
       <div className="relative mb-2 hidden w-[231px] md:block">
         <img className="h-[392px] w-[231px]" src={bgItemGalleryDesktop} alt="user's background" />
-        <img className="absolute top-[25px] left-[33px] h-[221px] w-[166px] shadow-inner" src={picture} alt="user's picture" />
+        <img className="absolute top-[25px] left-[33px] h-[221px] w-[166px]" src={picture} alt="user's picture" />
+        <div className="absolute top-[25px] left-[33px] h-[221px] w-[166px] border-2 opacity-30" />
         <div className="absolute left-4 top-[270px]">
           <span className="mb-1 block font-game text-xs capitalize">{firstName}</span>
           <span className="mb-2 block font-game text-xs capitalize">{lastName}</span>
