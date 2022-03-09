88d73cbbd42a18a838c1d9707485cb9a59294c16
Benjamin Lacroix <benjamin.lacroix@publicissapient.com>
2022-03-09 10:42:54 +0100

    feat/gallery v2 (#36)
    :sparkles: mobile & desktop gallery v2

--- src/components/Gallery/GalleryCard.tsx
+++ src/components/Gallery/GalleryCard.tsx
@@ -1,15 +1,46 @@
 import React, { FunctionComponent } from 'react';
 import bgItemGallery from '../../images/bg-item-gallery.png';
+import bgItemGalleryDesktop from '../../images/bg-item-gallery-desktop.png';
+import newComerBadge from '../../images/new-comer-badge.svg';
 import { User } from '../../services/gallery';
 
-export const GalleryCard: FunctionComponent<User> = ({ firstName, lastName, picture }) => (
-  <div className="relative w-[142px] text-center">
-    <img className="invisible w-full" src={bgItemGallery} alt="user's background" />
-    <img className="absolute inset-0 mt-[2px] w-full p-[10px]" src={picture} alt="user's picture" />
-    <img className="absolute inset-0 w-full" src={bgItemGallery} alt="user's background" />
-    <div className="absolute inset-x-0 bottom-[23px] leading-tight">
-      <div className="mt-1 font-game text-[8px]">{firstName}</div>
-      <div className="font-game text-[8px]">{lastName}</div>
-    </div>
-  </div>
-);
+export const GalleryCard: FunctionComponent<User> = ({ firstName, lastName, picture, capability, arrivalDate }) => {
+  const date = arrivalDate && new Date(arrivalDate);
+  const month = date && new Intl.DateTimeFormat('en-US', { month: 'long' }).format(date);
+  const year = date && date.getFullYear();
+  const isNewComer = date && Math.floor((new Date().getTime() - date.getTime()) / (1000 * 60 * 60 * 24)) < 90;
+  return (
+    <>
+      <div className="relative mb-2 w-[328px] md:hidden">
+        <img className="h-[149px] w-[328px]" src={bgItemGallery} alt="user's background" />
+        <img className="absolute top-[17px] left-[25px] h-[100px] w-[75px]" src={picture} alt="user's picture" />
+        <div className="absolute top-[17px] left-[25px] h-[100px] w-[75px] border-2 opacity-30" />
+        <div className="absolute left-32 top-4">
+          <span className="mb-1 block font-game text-xs capitalize">{firstName}</span>
+          <span className="mb-2 block font-game text-xs capitalize">{lastName}</span>
+          {capability && <span className="mb-2 block text-base text-blue-3">{capability}</span>}
+          {date && (
+            <span className="text-sm text-grey-2">
+              {month} {year} {isNewComer && <img className="-mt-1 inline" src={newComerBadge} />}
+            </span>
+          )}
+        </div>
+      </div>
+      <div className="relative mb-2 hidden w-[231px] md:block">
+        <img className="h-[392px] w-[231px]" src={bgItemGalleryDesktop} alt="user's background" />
+        <img className="absolute top-[25px] left-[33px] h-[221px] w-[166px]" src={picture} alt="user's picture" />
+        <div className="absolute top-[25px] left-[33px] h-[221px] w-[166px] border-2 opacity-30" />
+        <div className="absolute left-4 top-[270px]">
+          <span className="mb-1 block font-game text-xs capitalize">{firstName}</span>
+          <span className="mb-2 block font-game text-xs capitalize">{lastName}</span>
+          {capability && <span className="mb-2 block text-base text-blue-3">{capability}</span>}
+          {date && (
+            <span className="text-sm text-grey-2">
+              {month} {year} {isNewComer && <img className="-mt-1 inline" src={newComerBadge} />}
+            </span>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -10,7 +10,7 @@ interface ToolbarPropTypes {
 }
 
 export const Toolbar: FunctionComponent<ToolbarPropTypes> = ({ title, buttonLabel, link, state }) => (
-  <section className="relative sticky top-0 z-10 flex h-[48px] items-center justify-center bg-[#1F23A0] shadow-lg">
+  <section className="relative sticky top-0 z-20 flex h-[48px] items-center justify-center bg-[#1F23A0] shadow-lg">
     <Link to={link ? link : '/'} replace state={state}>
       <button className="absolute left-2 top-0 bottom-0 flex items-center font-game text-[8px] text-white">
         <img src={IcBack} height={16} width={16} /> {buttonLabel}
