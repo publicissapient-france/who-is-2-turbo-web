a8bf1f2d03743ab2bbf347e3883c4d784721515d
Arnaud Flaesch <ArnaudFlaesch@users.noreply.github.com>
2024-02-22 12:17:40 +0100

    Update layout (#75)

--- src/components/PlayChoice/PlayChoice.tsx
+++ src/components/PlayChoice/PlayChoice.tsx
@@ -8,6 +8,9 @@ export const PlayChoice = () => (
     <Metadata />
     <Toolbar title="Categories" buttonLabel="Back" />
     <div className="mx-auto mt-4 max-w-screen-xl flex flex-col gap-y-8 px-6 py-4 md:h-4/5 md:justify-center">
+      <section className="flex flex-row flex-wrap gap-x-8 gap-y-4 md:justify-center">
+        <SeriesButton series={'ALL'} />
+      </section>
       <section className="flex flex-row gap-x-8 md:justify-center">
         <SeriesButton series={'SERIES_5'} />
         <SeriesButton series={'SERIES_20'} />
@@ -19,9 +22,6 @@ export const PlayChoice = () => (
       <section className="flex flex-row max-w-screen-xl flex-wrap gap-x-8 gap-y-4 md:justify-center">
         <SeriesButton series={'EXPERIENCE'} />
         <SeriesButton series={'DATA'} />
-      </section>
-      <section className="flex flex-row flex-wrap gap-x-8 gap-y-4 md:justify-center">
-        <SeriesButton series={'ALL'} />
       </section>
     </div>
   </main>
