826f64b19ab6bfa679849b403a12ea3ddc7fcb21
Arnaud Flaesch <arnaud.flaesch@publicissapient.com>
2024-03-08 14:41:57 +0100

    fix

--- src/components/PlayChoice/PlayChoice.tsx
+++ src/components/PlayChoice/PlayChoice.tsx
@@ -7,7 +7,7 @@ export const PlayChoice = () => (
   <main className="h-screen">
     <Metadata />
     <Toolbar title="Categories" buttonLabel="Back" />
-    <div className="mx-auto mt-4 max-w-screen-xl flex flex-col gap-y-8 px-6 py-4 md:h-4/5 md:justify-center">
+    <div className="mx-auto mt-4  max-w-screen-xl flex flex-col gap-y-8 px-6 py-4 md:h-4/5 md:justify-center">
       <section className="flex flex-row flex-wrap gap-x-8 gap-y-4 md:justify-center">
         <SeriesButton series={'ALL'} />
       </section>
